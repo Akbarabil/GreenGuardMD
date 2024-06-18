44c5b1762f2376f33cdc7a9aac43146b3c6d7124
petraslent <124277889+petraslent@users.noreply.github.com>
2024-06-18 16:51:50 +0700

    perubahan tampilan main
    sepertinya kita harus pakai fragment

--- Capstone/app/src/main/java/com/example/greenguard/MainActivity.kt
+++ Capstone/app/src/main/java/com/example/greenguard/MainActivity.kt
@@ -5,16 +5,38 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    // Navigate to Home
+                    true
+                }
+                R.id.navigation_scanner -> {
+                    true
+                }
+                R.id.navigation_forum -> {
+                    // Navigate to Forum
+                    true
+                }
+                R.id.navigation_news -> {
+                    // Navigate to News
+                    true
+                }
+                R.id.navigation_profile -> {
+                    // Navigate to News
+                    true
+                }
+                else -> false
+            }
         }
     }
 }
