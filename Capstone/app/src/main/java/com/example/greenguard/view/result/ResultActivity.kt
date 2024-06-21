94d75d5156050a392bb32d90adb8bbae43799d7a
Ababil <96470039+Akbarabil@users.noreply.github.com>
2024-06-21 15:02:42 +0700

    intent ke result

--- Capstone/app/src/main/java/com/example/greenguard/view/result/ResultActivity.kt
+++ Capstone/app/src/main/java/com/example/greenguard/view/result/ResultActivity.kt
@@ -6,16 +6,27 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.greenguard.R
+import com.example.greenguard.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityResultBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_result)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun enableEdgeToEdge() {
+        // Your implementation of enableEdgeToEdge()
     }
 }
